3d9468cc3d77bf713af92448dc0f81b545487e1c
I Putu Ananta Yogiswara <ananta.putu@gmail.com>
2018-04-03 21:45:04 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile v-for="item in menuItems" :key="item.title" :to="item.link">
+          <v-list-tile-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{item.title}}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+      :clipped-left="clipped"
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">Hydro App</router-link>  
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only" >
+        <v-btn flat v-for="item in menuItems" :key="item.title">
+          <v-icon left>{{item.icon}}</v-icon>
+          {{item.title}}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: true,
+      menuItems: [{
+        icon: 'create',
+        title: 'Create Reservoir'
+      }, {
+        icon: 'account_box',
+        title: 'Profile'
+      }, {
+        icon: 'lock_open',
+        title: 'Login'
+      }, {
+        icon: 'supervisor_account',
+        title: 'Sign Up'
+      }],
+      miniVariant: false,
+      title: 'Vuetify.js'
+    }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs-12 sm6 offset-sm3>
+        <v-card>
+          <v-card-media src="https://www.growingmagazine.com/wp-content/uploads/2017/05/analyzing-future-hydroponics-image.jpg" height="200px">
+          </v-card-media>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs-12 sm6 offset-sm3>
+        <v-card>
+          <v-card-media src="https://www.growingmagazine.com/wp-content/uploads/2017/05/analyzing-future-hydroponics-image.jpg" height="200px">
+          </v-card-media>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
